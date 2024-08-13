0d8a15dba9339fc75fe23d855117734c25e44f10
radiol <radiol@github.com>
2024-08-13 12:33:49 +0900

    Add initial project files and main functionality

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use clipboard::{ClipboardContext, ClipboardProvider};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use regex::Regex;
+use std::fs;
+use std::path::Path;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+
+#[derive(Debug, serde::Deserialize)]
+struct Replacement {
+    original: String,
+    replacement: String,
+}
+
+fn load_replacements(file_path: &str) -> Vec<Replacement> {
+    let data = fs::read_to_string(file_path).expect("Failed to read file");
+    serde_json::from_str(&data).expect("Failed to parse JSON")
+}
+
+fn format_text(text: &str, replacements: &[Replacement]) -> String {
+    let mut formatted_content = text.to_string();
+    for replacement in replacements {
+        formatted_content =
+            formatted_content.replace(&replacement.original, &replacement.replacement);
+    }
+    let re = Regex::new(r"[！-～]").unwrap();
+    formatted_content = re
+        .replace_all(&formatted_content, |caps: &regex::Captures| {
+            let c = caps[0].chars().next().unwrap();
+            let half_width_cahr = (c as u32 - 0xfee0) as u8 as char;
+            half_width_cahr.to_string()
+        })
+        .to_string();
+    formatted_content
+}
+fn main() {
+    let mut replacements = load_replacements("replacements.json");
+    let (tx, rx) = channel();
+    let config = Config::default().with_poll_interval(Duration::from_secs(2));
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, config).unwrap();
+    watcher
+        .watch(Path::new("replacements.json"), RecursiveMode::NonRecursive)
+        .unwrap();
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+
+    loop {
+        let clipboard_content = ctx.get_contents().unwrap_or_default();
+        let formatted_content = format_text(&clipboard_content, &replacements);
+        if clipboard_content != formatted_content {
+            println!("Replace {} to {}", clipboard_content, formatted_content);
+            ctx.set_contents(formatted_content).unwrap();
+        }
+
+        if rx.try_recv().is_ok() {
+            println!("replacements.json has been modified.");
+            println!("Reloading replacements...");
+            replacements = load_replacements("replacements.json");
+        }
+
+        thread::sleep(Duration::from_secs(1));
+    }
+}
